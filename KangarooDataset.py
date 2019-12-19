adb12818f3307bd657744768a07943949858d11c
Rosario Laface <rosario.laface@nttdata.com>
2019-12-19 15:21:23 +0100

    adding main

--- KangarooDataset.py
+++ KangarooDataset.py
@@ -89,15 +89,3 @@ class KangarooDataset(Dataset):
 		info = self.image_info[image_id]
 		return info['path']
 
-# train set
-dataset_dir = '/home/scripts/samples'
-train_set = KangarooDataset()
-train_set.load_dataset('kangaroo', is_train=True)
-train_set.prepare()
-print('Train: %d' % len(train_set.image_ids))
-
-# test/val set
-test_set = KangarooDataset()
-test_set.load_dataset('kangaroo', is_train=False)
-test_set.prepare()
-print('Test: %d' % len(test_set.image_ids))
