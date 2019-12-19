8c3aaf2784db214258d7e080f8a05c518ff6529b
Rosario Laface <rosario.laface@nttdata.com>
2019-12-19 15:21:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from KangarooDataset import extract_boxes, KangarooDataset
 
-# train set
-dataset_dir = '/home/scripts/samples'
-train_set = KangarooDataset()
-train_set.load_dataset('kangaroo', is_train=True)
-train_set.prepare()
-print('Train: %d' % len(train_set.image_ids))
+def main():
+    # train set
+    dataset_dir = '/home/scripts/samples'
+    train_set = KangarooDataset()
+    train_set.load_dataset('kangaroo', is_train=True)
+    train_set.prepare()
+    print('Train: %d' % len(train_set.image_ids))
 
-# test/val set
-test_set = KangarooDataset()
-test_set.load_dataset('kangaroo', is_train=False)
-test_set.prepare()
-print('Test: %d' % len(test_set.image_ids))
+    # test/val set
+    test_set = KangarooDataset()
+    test_set.load_dataset('kangaroo', is_train=False)
+    test_set.prepare()
+    print('Test: %d' % len(test_set.image_ids))
+
+if __name__ == "__main__":
+    main()
