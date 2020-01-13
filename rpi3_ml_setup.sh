f19a93adc75c026a9907cdd1cd4e06d4eddfd3bf
Rosario Laface <rosario.laface@nttdata.com>
2020-01-13 11:53:59 +0100

    Update rpi3_ml_setup.sh

--- rpi3_ml_setup.sh
+++ rpi3_ml_setup.sh
@@ -2,7 +2,7 @@ mkdir ~/Codice \
 && cd ~/Codice \
 && wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh \
 && sudo /bin/bash Miniconda3-latest-Linux-armv7l.sh \ # -> change default directory to /home/pi/miniconda3
-&& sudo echo export PATH="/home/pi/miniconda3/bin:$PATH" >> /home/pi/.bashrc \
+&& echo "export PATH=/home/pi/miniconda3/bin:$PATH" >> /home/pi/.bashrc \
 && sudo apt-get update \
 && sudo apt install -y libatlas-base-dev \
 && pip3 install tensorflow \
@@ -20,7 +20,7 @@ mkdir ~/Codice \
 && sudo apt install -y python3-opencv \
 && curl -sSL https://get.docker.com | sh \
 && sudo usermod -aG docker pi \
-&& export PATH=~/.local/bin/jupyter-notebook:$PATH \
+&& echo "export PATH=~/.local/bin:$PATH" >> /home/pi/.bashrc \
 && sudo rm ~/Codice/Miniconda3-latest-Linux-armv7l.sh \
 && mkdir ~/Codice/notebooks \
 && sudo reboot -h now
