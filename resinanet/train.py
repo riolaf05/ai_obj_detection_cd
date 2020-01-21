9ec909cb6c872fd3ccbc13bffd1aebab9609958a
Rosario Laface <rosario.laface@nttdata.com>
2020-01-21 17:25:25 +0100

    adding batch object detection with resinanet

---
+++ resinanet/train.py
@@ -0,0 +1,125 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from pandas.plotting import register_matplotlib_converters
+from sklearn.model_selection import train_test_split
+import urllib
+import os
+import csv
+import cv2
+import time
+from PIL import Image
+
+from keras_retinanet import models
+from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_retinanet.utils.visualization import draw_box, draw_caption
+from keras_retinanet.utils.colors import label_color
+
+def show_image_objects(image_row):
+
+  img_path = image_row.image_name
+  box = [
+    image_row.x_min, image_row.y_min, image_row.x_max, image_row.y_max
+  ]
+
+  image = read_image_bgr(img_path)
+
+  draw = image.copy()
+  draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+  draw_box(draw, box, color=(255, 255, 0))
+
+  plt.axis('off')
+  plt.imshow(draw)
+  plt.show()
+
+%matplotlib inline
+%config InlineBackend.figure_format='retina'
+
+register_matplotlib_converters()
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+
+rcParams['figure.figsize'] = 22, 10
+
+RANDOM_SEED = 42
+
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+plates_df = pd.read_json('indian_number_plates.json', lines=True)
+
+dataset = dict()
+dataset["image_name"] = list()
+dataset["x_min"] = list()
+dataset["y_min"] = list()
+dataset["x_max"] = list()
+dataset["y_max"] = list()
+dataset["class_name"] = list()
+
+counter = 0
+for index, row in plates_df.iterrows():
+    img = urllib.request.urlopen(row["content"])
+    img = Image.open(img)
+    img = img.convert('RGB')
+    img.save(f'number_plates/licensed_car_{counter}.jpeg', "JPEG")
+    
+    dataset["image_name"].append(f'number_plates/licensed_car_{counter}.jpeg')
+    
+    data = row["annotation"]
+  
+    width = data[0]["imageWidth"]
+    height = data[0]["imageHeight"]
+
+    dataset["x_min"].append(int(round(data[0]["points"][0]["x"] * width)))
+    dataset["y_min"].append(int(round(data[0]["points"][0]["y"] * height)))
+    dataset["x_max"].append(int(round(data[0]["points"][1]["x"] * width)))
+    dataset["y_max"].append(int(round(data[0]["points"][1]["y"] * height)))
+    dataset["class_name"].append("license_plate")
+    
+    counter += 1
+print("Downloaded {} car images.".format(counter))
+
+#Preprocessing
+
+train_df, test_df = train_test_split(
+  df, 
+  test_size=0.2, 
+  random_state=RANDOM_SEED
+)
+
+ANNOTATIONS_FILE = 'annotations.csv'
+CLASSES_FILE = 'classes.csv'
+
+train_df.to_csv(ANNOTATIONS_FILE, index=False, header=None)
+
+classes = set(['license_plate'])
+
+with open(CLASSES_FILE, 'w') as f:
+  for i, line in enumerate(sorted(classes)):
+    f.write('{},{}\n'.format(line,i))
+
+PRETRAINED_MODEL = './snapshots/_pretrained_model.h5'
+
+URL_MODEL = 'https://github.com/fizyr/keras-retinanet/releases/download/0.5.1/resnet50_coco_best_v2.1.0.h5'
+urllib.request.urlretrieve(URL_MODEL, PRETRAINED_MODEL)
+
+print('Downloaded pretrained model to ' + PRETRAINED_MODEL)
+
+#Train
+
+!keras_retinanet/bin/train.py --freeze-backbone --random-transform --weights {PRETRAINED_MODEL} --batch-size 8 --steps 500 --epochs 1 csv annotations.csv classes.csv
+
+#Load model
+
+model_path = os.path.join('snapshots', sorted(os.listdir('snapshots'), reverse=True)[0])
+print(model_path)
+
+model = models.load_model(model_path, backbone_name='resnet50')
+model = models.convert_model(model)
+
+labels_to_names = pd.read_csv(CLASSES_FILE, header=None).T.loc[0].to_dict()
