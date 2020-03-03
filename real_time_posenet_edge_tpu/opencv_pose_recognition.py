fff9b0d8a206f2d7b8482bb3710c499879267030
Rosario Laface <rosario.laface@nttdata.com>
2020-03-03 17:22:46 +0100

    adding flask server

--- real_time_posenet_edge_tpu/opencv_pose_recognition.py
+++ real_time_posenet_edge_tpu/opencv_pose_recognition.py
@@ -1,10 +1,15 @@
 #import tensorflow as tf
+from flask import Flask, render_template, Response
 import cv2
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
+#lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+
+def pose_detect():
     #with tf.Session() as sess:
         #model_cfg, model_outputs = posenet.load_model(args.model, sess)
         #output_stride = model_cfg['output_stride']
@@ -40,16 +45,30 @@ def main():
             poses, inference_time = engine.DetectPosesInImage(np.uint8(pil_image))
             print('Inference time: %.fms' % inference_time)
 
+            '''
             for pose in poses:
                 if pose.score < 0.4: continue
                 print('\nPose Score: ', pose.score)
                 for label, keypoint in pose.keypoints.items():
                     print(' %-20s x=%-4d y=%-4d score=%.1f' %
                         (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
-
+            '''
+            encode_return_code, image_buffer = cv2.imencode('.jpg', frame) #TODO: draw lines on body parts!
+            io_buf = io.BytesIO(image_buffer)
+            yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + io_buf.read() + b'\r\n')
             #cv2.imshow("video", frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+
+@app.route('/pose_detection')
+def pose_detection():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(
+        pose_detect(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', debug=True, threaded=True)
