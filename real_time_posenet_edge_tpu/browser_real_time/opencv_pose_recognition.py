1dce72d8e61136c28a5c067848f7d6c7aa4bba14
Rosario Laface <rosario.laface@nttdata.com>
2020-03-08 20:25:28 +0100

    Update opencv_pose_recognition.py

--- real_time_posenet_edge_tpu/browser_real_time/opencv_pose_recognition.py
+++ real_time_posenet_edge_tpu/browser_real_time/opencv_pose_recognition.py
@@ -146,16 +146,26 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     engine = PoseEngine('/home/scripts/pose_detection/browser/models/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
                     poses, inference_time = engine.DetectPosesInImage(np.uint8(pil_im))
 
-                    #print point on shell
+                    #print points on shell
                     '''
                     for pose in poses:
                         if pose.score < 0.4: continue
                         print('\nPose Score: ', pose.score)
                         for label, keypoint in pose.keypoints.items():
-                            print(' %-20s x=%-4d y=%-4d score=%.1f' %
-                                (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))                    
+                            #print(' %-20s x=%-4d y=%-4d score=%.1f' %
+                                #(label, keypoint.yx[1], keypoint.yx[0], keypoint.score))          
+                    #a = np.array(points)
                     '''
-                    
+
+                    points=[]
+                    for pose in poses:
+                        if pose.score < 0.4: 
+                            for part in pose.keypoints.keys():
+                                points.append([pose.keypoints[part].yx[1], pose.keypoints[part].yx[1]])
+                    a = np.array(points)
+                    if a != []:
+                        for coord in a: 
+                            cv2.line(cv2_im, a[0], a[1], [0, 255, 0], 2)
                     r, buf = cv2.imencode(".jpg", cv2_im)
 
                     self.wfile.write(b'--FRAME\r\n')
