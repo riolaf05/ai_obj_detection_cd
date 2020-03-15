79d5c229cc285317c04e0c7971f633ea356616bc
Rosario Laface <rosario.laface@nttdata.com>
2020-03-15 17:50:37 +0100

    adding config pipeline and retrain scripts

--- real_time_object_detection_edge_tpu/object_detection_transfer_learning/models/download.sh
+++
@@ -1,4 +0,0 @@
-!git clone https://github.com/riolaf05/ai_obj_detection_cd/
-!git -C ai_obj_detection_cd checkout object-detection-retrain
-%cd /content/ai_obj_detection_cd/real_time_object_detection_edge_tpu/object_detection_transfer_learning
-%ls
