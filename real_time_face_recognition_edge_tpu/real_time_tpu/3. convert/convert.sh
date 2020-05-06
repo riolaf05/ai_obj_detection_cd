7af94393f7ff717f5435411d5d6f44fe3ba17192
Rosario Laface <rosario.laface@nttdata.com>
2020-05-06 18:50:32 +0200

    adding face recognition on tpu facenet

---
+++ real_time_face_recognition_edge_tpu/real_time_tpu/3. convert/convert.sh
@@ -0,0 +1,5 @@
+tflite_convert \
+  --output_file=/tmp/frozen_mobilenet_v1_l2norm_optimized.tflite \
+  --graph_def_file=/tmp/frozen_mobilenet_v1_l2norm_optimized.pb \
+  --input_arrays=input \
+  --output_arrays=MobilenetV1/Predictions/Reshape_1
