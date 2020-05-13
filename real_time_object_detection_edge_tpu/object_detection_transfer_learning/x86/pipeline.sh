153235caf1521b259a4e1d464f9e45f0ad588e27
Rosario Laface <rosario.laface@nttdata.com>
2020-05-13 16:17:26 +0200

    Update pipeline.sh

--- real_time_object_detection_edge_tpu/object_detection_transfer_learning/x86/pipeline.sh
+++ real_time_object_detection_edge_tpu/object_detection_transfer_learning/x86/pipeline.sh
@@ -1,2 +1,14 @@
+#Resize images
+python transform_image_resolution.py -d data/images/ -s 800 600
+#Convert XML to CSV
+python3 xml_to_csv.py -i data/images/train -o data/annotations/train_labels.csv
+python3 xml_to_csv.py -i data/images/test -o data/annotations/test_labels.csv
+#Generate TFRecords
+python3 generate_tfrecord.py --csv_input=data/annotations/train_labels.csv --image_dir=data/images  --output_path=data/annotations/train.record
+python3 generate_tfrecord.py --csv_input=data/annotations/test_labels.csv --image_dir=data/images  --output_path=data/annotations/test.record
+#Run preprocess step
 mlflow run preprocess/ -b local --no-conda -e preprocess
+#Run train step
 mlflow run training/ -b local --no-conda -e train config_file='ssd_mobilenet_v2_coco.config' num_train_steps=500 num_eval_steps=500
+#Run conversion step
+#TODO
