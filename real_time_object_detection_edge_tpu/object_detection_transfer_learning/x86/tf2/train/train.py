bc5148bbb1ebc56e6fa251329f1f914dc91bcdc4
Rosario Laface <rosario.laface@nttdata.com>
2020-05-26 15:34:50 +0200

    Update train.py

--- real_time_object_detection_edge_tpu/object_detection_transfer_learning/x86/tf2/train/train.py
+++ real_time_object_detection_edge_tpu/object_detection_transfer_learning/x86/tf2/train/train.py
@@ -89,7 +89,7 @@ def train(base_dir, image_size, batch_size, epochs):
     metrics = {
         'metrics': [{
         'name': 'accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
-        'numberValue':  accuracy, # The value of the metric. Must be a numeric value.
+        'numberValue':  acc, # The value of the metric. Must be a numeric value.
         'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
         }]
     }
