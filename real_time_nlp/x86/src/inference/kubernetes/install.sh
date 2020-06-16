35b6a449f78cdf6c02b874cf1a1b786671035871
Rosario Laface <rosario.laface@nttdata.com>
2020-06-16 12:55:59 +0200

    Update install.sh

--- real_time_nlp/x86/src/inference/kubernetes/install.sh
+++ real_time_nlp/x86/src/inference/kubernetes/install.sh
@@ -3,4 +3,4 @@ mkdir ~/chatbot \
 && kubectl apply -f pv.yaml && kubectl apply -f pvc.yaml && kubectl apply -f chatbot.yaml 
 
 #to delete all
-#kubectl delete deployment chatbot && kubectl delete pcv chatbot-pvc && kubectl delete pv chatbot-pv
+#kubectl delete deployment chatbot && kubectl delete pvc chatbot-pvc && kubectl delete pv chatbot-pv
