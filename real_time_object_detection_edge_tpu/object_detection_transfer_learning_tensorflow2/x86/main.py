5a5cc703c0d9f07be8049471e421da5cafd2e58e
Rosario Laface <rosario.laface@nttdata.com>
2020-10-07 13:49:25 +0200

    Update main.py

--- real_time_object_detection_edge_tpu/object_detection_transfer_learning_tensorflow2/x86/main.py
+++ real_time_object_detection_edge_tpu/object_detection_transfer_learning_tensorflow2/x86/main.py
@@ -35,6 +35,71 @@ def feature_extractor(x):
   feature_extractor_module = hub.Module(feature_extractor_url)
   return feature_extractor_module(x)
 
+def keras_save_model(self, model, model_dir='/tmp'):
+        """
+        Convert Keras estimator to TensorFlow
+        :type model_dir: object
+        """
+        print("Model is saved locally to %s" % model_dir)
+        mlflow.tensorflow.save_model(model, model_dir)
+
+def keras_save_model(self, model, model_dir='/tmp'):
+        """
+        Convert Keras estimator to TensorFlow
+        :type model_dir: object
+        """
+        print("Model is saved locally to %s" % model_dir)
+        mlflow.keras.save_model(model, model_dir)
+
+
+def evaluate_model(model, x_test, y_test):
+    """
+    Evaluate the model with unseen and untrained data
+    :param model:
+    :return: results of probability
+    """
+
+    return model.evaluate(x_test, y_test)
+
+def get_binary_loss(hist):
+    loss = hist.history['loss']
+    loss_val = loss[len(loss) - 1]
+    return loss_val
+
+def get_binary_acc(hist):
+    acc = hist.history['accuracy']
+    acc_value = acc[len(acc) - 1]
+
+    return acc_value
+
+def get_validation_loss(hist):
+    val_loss = hist.history['val_loss']
+    val_loss_value = val_loss[len(val_loss) - 1]
+
+    return val_loss_value
+
+def get_validation_acc(hist):
+    val_acc = hist.history['val_accuracy']
+    val_acc_value = val_acc[len(val_acc) - 1]
+
+    return val_acc_value
+
+
+def print_metrics(hist):
+
+    acc_value = get_binary_acc(hist)
+    loss_value = get_binary_loss(hist)
+
+    val_acc_value = get_validation_acc(hist)
+
+    val_loss_value = get_validation_loss(hist)
+
+    print("Final metrics: binary_loss:%6.4f" % loss_value)
+    print("Final metrics: binary_accuracy=%6.4f" % acc_value)
+    print("Final metrics: validation_binary_loss:%6.4f" % val_loss_value)
+    print("Final metrics: validation_binary_accuracy:%6.4f" % val_acc_value)
+
+
 #create a custom callback to visualize the training progress during every epoch.
 class CollectBatchStats(tf.keras.callbacks.Callback):
   def __init__(self):
@@ -54,6 +119,7 @@ def main():
     parser.add_argument('--steps-per-epoch', type=int, help='Steps per Epochs', default=21)
     parser.add_argument('--experiment', type=str, help='Experiment name', default='default')
     parser.add_argument('--version', type=str, help='Experiment version', default='latest')
+    parser.add_argument('--loss', type=str, help='Loss function', default='categorical_crossentropy')
     args = parser.parse_args() 
 
     #Mlflow settings
@@ -64,11 +130,7 @@ def main():
         exp_id = mlflow.set_experiment(args.experiment)
     else: 
         exp_id = mlflow.create_experiment(args.experiment)
-    #Set tags
-    tags={}
-    tags['name']=args.experiment
-    tags['version']=args.version
-    mlflow.set_tags(tags)
+    
     #Close active runs
     if mlflow.active_run():
         mlflow.end_run()
@@ -109,7 +171,7 @@ def main():
 
     model.compile(
         optimizer = tf.train.AdamOptimizer(),
-        loss = "categorical_crossentropy",
+        loss = args.loss,
         metrics = ['accuracy']
         )
 
@@ -120,10 +182,42 @@ def main():
     batch_stats = CollectBatchStats()
 
     with mlflow.start_run(run_id=None, experiment_id=exp_id, run_name=None, nested=False): 
+
       # fitting the model
-      model.fit((item for item in train_image_data), epochs = args.epochs,
+      history = model.fit((item for item in train_image_data), epochs = args.epochs,
               steps_per_epoch=21,
               callbacks = [batch_stats, es],validation_data=test_image_data)
+
+      #mlflow autolog
+      #mlflow.tensorflow.autolog()
+
+      #Set tags
+      tags={}
+      tags['name']=args.experiment
+      tags['version']=args.version
+      mlflow.set_tags(tags)
+
+      #mlflow logging
+      # log parameters
+      #mlflow.log_param("hidden_layers", args.hidden_layers)
+      #mlflow.log_param("output", args.output)
+      mlflow.log_param("epochs", args.epochs)
+      mlflow.log_param("loss_function", args.loss)
+      # log metrics
+      mlflow.log_metric("binary_loss", get_binary_loss(history))
+      mlflow.log_metric("binary_acc",  get_binary_acc(history))
+      mlflow.log_metric("validation_loss", get_binary_loss(history))
+      mlflow.log_metric("validation_acc", get_validation_acc(history))
+      #mlflow.log_metric("average_loss", results[0])
+      #mlflow.log_metric("average_acc", results[1])
+      
+      # log artifacts (matplotlib images for loss/accuracy)
+      #mlflow.log_artifacts(image_dir)
+      #log model
+      t = time.time()
+      #model_dir = os.path.join(BASE_DIR, "models"+"{}".format(int(t)))
+      #keras_save_model(model, model_dir)
+      #mlflow.tensorflow.log_model(keras_model, model_dir)
 
       #print labels
       label_names = sorted(train_image_data.class_indices.items(), key=lambda pair:pair[1])
@@ -135,13 +229,9 @@ def main():
       labels_batch = label_names[np.argmax(result_batch, axis=-1)]
       print(labels_batch)
 
-      mlflow.tensorflow.autolog()
-
-      #save model 
-      t = time.time()
-      export_path = os.path.join(BASE_DIR, "models"+"{}".format(int(t)))
-      model.save(export_path)
-      mlflow.tensorflow.log_model(model, "model", registered_model_name=args.expertiment+"_"+args.version) 
+      #export model
+      #model.save(export_path)
+      #mlflow.tensorflow.log_model(model, "model", registered_model_name=args.expertiment+"_"+args.version) 
 
       mlflow.end_run()
 
