c98e2da12be8c658d3550322356f35e705b53e8b
Rosario Laface <rosario.laface@nttdata.com>
2020-10-08 10:33:17 +0200

    fix on convert job

--- real_time_object_detection_edge_tpu/object_detection_transfer_learning_tensorflow2/x86/convert.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.contrib import lite
-
-converter = lite.TFLiteConverter.from_keras_model_file( r'models\\1602079553.h5')
-tfmodel = converter.convert()
-
-open(r"models\\1602079553.tflite" , "wb").write(tfmodel)
